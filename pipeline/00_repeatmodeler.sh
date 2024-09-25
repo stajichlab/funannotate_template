b3c68c90c8711057903b2f4110d3d785e347b936
Jason Stajich <jason.stajich@ucr.edu>
2024-09-24 21:59:57 -0300

    Update 00_repeatmodeler.sh
    untested - repeatmodeler run update

--- pipeline/00_repeatmodeler.sh
+++ pipeline/00_repeatmodeler.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash -l
-#SBATCH -p batch --time 2-0:00:00 --ntasks 8 --nodes 1 --mem 120G --out logs/repeatmodeler_attempt.%a.log
+#SBATCH --time 2-0:00:00 -N 1 -n 1 -c 16 --mem 64gb --out logs/repeatmodeler.%a.log
 
+module load RepeatModeler
 CPU=1
 if [ $SLURM_CPUS_ON_NODE ]; then
     CPU=$SLURM_CPUS_ON_NODE
 fi
 
 INDIR=genomes
+MASKDIR=analysis/RepeatMasker
+SAMPLES=samples.csv
+RMLIBFOLDER=lib/repeat_library
+FUNGILIB=lib/fungi_repeat.20170127.lib.gz
+mkdir -p $RMLIBFOLDER
+RMLIBFOLDER=$(realpath $RMLIBFOLDER)
+
 OUTDIR=genomes
 
 mkdir -p repeat_library
@@ -14,9 +22,9 @@ mkdir -p repeat_library
 SAMPFILE=samples.csv
 N=${SLURM_ARRAY_TASK_ID}
 
-if [ ! $N ]; then
+if [ -z $N ]; then
     N=$1
-    if [ ! $N ]; then
+    if [ -z $N ]; then
         echo "need to provide a number by --array or cmdline"
         exit
     fi
@@ -29,19 +37,24 @@ if [ $N -gt $(expr $MAX) ]; then
 fi
 
 IFS=,
-tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN PHYLUM LOCUS
-do
-  name=$(echo -n ${SPECIES}_${STRAIN} | perl -p -e 's/\s+/_/g')
-  echo "$name"
-     module unload perl
-     module unload python
-     module unload miniconda2
-     module unload anaconda3
-     module load RepeatModeler
-     module load ncbi-blast/2.13.0+
-     export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
-	#makeblastdb -in $INDIR/$name.sorted.fasta -dbtype nucl -out repeat_library/$name
-	BuildDatabase -name repeat_library/$name $INDIR/$name.sorted.fasta
-	RepeatModeler -database repeat_library/$name -pa $CPU
-	#-LTRStruct
+# this may change depending on your samples file configuration
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN PHYLUM LOCUS RNASEQ
+do  
+  SPECIESNOSPACE=$(echo -n "$SPECIES $STRAIN" | perl -p -e 's/\s+/_/g')
+  LIBRARY=$RMLIBFOLDER/$SPECIESNOSPACE.repeatmodeler.lib
+  COMBOLIB=$RMLIBFOLDER/$SPECIESNOSPACE.combined.lib
+  
+  if [ ! -s $LIBRARY ]; then
+  	pushd $MASKDIR/$SPECIESNOSPACE
+	BuildDatabase -name $SPECIESNOSPACE $GENOME
+	RepeatModeler -threads $CPU -database $SPECIESNOSPACE -LTRStruct
+	rsync -a RM_*/consensi.fa.classified $LIBRARY
+	rsync -a RM_*/families-classified.stk $RMLIBFOLDER/$SPECIESNOSPACE.repeatmodeler.stk
+ 	pigz $RMLIBFOLDER/$SPECIESNOSPACE.repeatmodeler.stk
+	popd
+  fi
+  if [ ! -s $COMBOLIB ]; then
+  	rsync -a $LIBRARY $COMBOLIB
+	zcat $FUNGILIB >> $COMBOLIB
+  fi
 done
